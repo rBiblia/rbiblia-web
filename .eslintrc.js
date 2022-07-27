f0a3394e9695c10ce8e3d03f104a6a8239364949
Jakub A. W <jakubwasek@gmail.com>
2022-07-27 19:04:40 +0200

    Dynamic language - change book names for language specific

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'prettier'
     ],
+    plugins: ['prettier'],
     parserOptions: {
         ecmaVersion: 6,
         sourceType: 'module',
@@ -17,7 +19,9 @@ module.exports = {
     },
     rules: {
         "no-console": 0,
-        "no-unused-vars": 0
+        "no-unused-vars": 0,
+        "react/prop-types": 0,
+        "prettier/prettier": ["error"]
     },
     settings: {
         react: {
