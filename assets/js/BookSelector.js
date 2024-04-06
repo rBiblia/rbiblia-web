20255ff37cf5e560a018df8c04b08d019004fd5f
Jakub A. W <jakubwasek@gmail.com>
2024-04-06 16:51:16 +0200

    refactored a few class components to functional components

--- assets/js/BookSelector.js
+++ assets/js/BookSelector.js
@@ -1,47 +1,39 @@
-import React, { Component } from "react";
-import { injectIntl } from "react-intl";
+import React from "react";
+import { useIntl } from "react-intl";
 
-class BookSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.onSelect = this.onSelect.bind(this);
-    }
+const BookSelector = ({
+    books,
+    structure,
+    isStructureLoaded,
+    selectedBook,
+    changeSelectedBook,
+}) => {
+    const { formatMessage } = useIntl();
+    const onSelect = (event) => {
+        changeSelectedBook(event.target.value);
+    };
 
-    onSelect(event) {
-        return this.props.changeSelectedBook(event.target.value);
-    }
-
-    render() {
-        const {
-            books,
-            structure,
-            isStructureLoaded,
-            selectedBook,
-            intl: { formatMessage },
-        } = this.props;
-
-        if (!isStructureLoaded) {
-            return (
-                <select className="form-control selector-disabled">
-                    <option>{formatMessage({ id: "bookList" })}</option>
-                </select>
-            );
-        }
-
+    if (!isStructureLoaded) {
         return (
-            <select
-                className="form-control"
-                onChange={this.onSelect}
-                value={selectedBook}
-            >
-                {Object.keys(structure).map((bookId) => (
-                    <option value={bookId} key={bookId}>
-                        {books[bookId].name}
-                    </option>
-                ))}
+            <select className="form-control selector-disabled">
+                <option>{formatMessage({ id: "bookList" })}</option>
             </select>
         );
     }
-}
 
-export default injectIntl(BookSelector);
+    return (
+        <select
+            className="form-control"
+            onChange={onSelect}
+            value={selectedBook}
+        >
+            {Object.keys(structure).map((bookId) => (
+                <option value={bookId} key={bookId}>
+                    {books[bookId].name}
+                </option>
+            ))}
+        </select>
+    );
+};
+
+export default BookSelector;
--- assets/js/ChapterSelector.js
+++ assets/js/ChapterSelector.js
@@ -1,47 +1,39 @@
-import { formatMessage } from "@formatjs/intl";
-import React, { Component } from "react";
-import { injectIntl } from "react-intl";
+import React from "react";
+import { useIntl } from "react-intl";
 
-class ChapterSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.onSelect = this.onSelect.bind(this);
-    }
+const ChapterSelector = ({
+    chapters,
+    isStructureLoaded,
+    selectedChapter,
+    changeSelectedChapter,
+}) => {
+    const { formatMessage } = useIntl();
 
-    onSelect(event) {
-        return this.props.changeSelectedChapter(event.target.value);
-    }
+    const onSelect = (event) => {
+        changeSelectedChapter(event.target.value);
+    };
 
-    render() {
-        const {
-            chapters,
-            isStructureLoaded,
-            selectedChapter,
-            intl: { formatMessage },
-        } = this.props;
-
-        if (isStructureLoaded && chapters && chapters.length) {
-            return (
-                <select
-                    className="form-control"
-                    onChange={this.onSelect}
-                    value={selectedChapter}
-                >
-                    {chapters.map((chapterId) => (
-                        <option value={chapterId} key={chapterId}>
-                            {chapterId}
-                        </option>
-                    ))}
-                </select>
-            );
-        }
-
+    if (isStructureLoaded && chapters && chapters.length) {
         return (
-            <select className="form-control selector-disabled">
-                <option>{formatMessage({ id: "chapterList" })}</option>
+            <select
+                className="form-control"
+                onChange={onSelect}
+                value={selectedChapter}
+            >
+                {chapters.map((chapterId) => (
+                    <option value={chapterId} key={chapterId}>
+                        {chapterId}
+                    </option>
+                ))}
             </select>
         );
     }
-}
 
-export default injectIntl(ChapterSelector);
+    return (
+        <select className="form-control selector-disabled">
+            <option>{formatMessage({ id: "chapterList" })}</option>
+        </select>
+    );
+};
+
+export default ChapterSelector;
