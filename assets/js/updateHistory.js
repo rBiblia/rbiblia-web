8f102981bb8017fbb50f30b691edbd2dd9f31233
Maciej Wajdzik <maciej.wajdzik@gmail.com>
2026-03-22 00:54:15 +0100

    fix: restore scroll position after page reload from search results

--- assets/js/updateHistory.js
+++ assets/js/updateHistory.js
@@ -6,7 +6,7 @@ import { COOKIE_EXPIRES, URL_PREFIX } from "../consts";
  * The history (url path) should be updated when
  * the last call is finished and verses are ready to be displayed
  */
-function updateHistory(language, translation, book, chapter) {
+function updateHistory(language, translation, book, chapter, verse = null) {
     Cookies.set("recent_language", language, { expires: COOKIE_EXPIRES });
     Cookies.set("recent_translation", translation, {
         expires: COOKIE_EXPIRES,
@@ -14,10 +14,12 @@ function updateHistory(language, translation, book, chapter) {
     Cookies.set("recent_book", book, { expires: COOKIE_EXPIRES });
     Cookies.set("recent_chapter", chapter, { expires: COOKIE_EXPIRES });
 
-    window.history.pushState(
+    const hash = verse ? `#${verse}` : "";
+
+    globalThis.history.pushState(
         {},
         "",
-        `${URL_PREFIX}/${language}/${translation}/${book}/${chapter}`
+        `${URL_PREFIX}/${language}/${translation}/${book}/${chapter}${hash}`
     );
 }
 
